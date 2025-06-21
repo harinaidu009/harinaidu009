9bf6574648ef3058f1553e1338ef1dcde0f4b6dd
harinaidu009 <haribaswa999@gmail.com>
2025-06-21 10:29:58 +0530

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Arr{
+	public static void main(String[] args)
+	{
+		ArrayList<String>al=new ArrayList<String>();
+		al.add("apple");
+		al.add("mango");
+		al.add("watermelon");
+		System.out.println(al);
+	}
+
+}
